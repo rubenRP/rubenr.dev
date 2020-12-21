3f0761a7ff6f25409950a29406abfd1548553ad7
Ruben Rodriguez <rubenr@MacMini.local>
2020-12-21 09:29:22 +0100

    Newsletter integration

---
+++ src/components/Newsletter/index.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+
+const Newsletter: React.FC = () => {
+  const processForm = form => {
+    const data = new FormData(form)
+    data.append("form-name", "newsletter")
+    fetch("/", {
+      method: "POST",
+      body: data,
+    })
+      .then(() => {
+        form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`
+      })
+      .catch(error => {
+        form.innerHTML = `<div class="form--error">Error: ${error}</div>`
+      })
+  }
+  const emailForm = document.querySelector(".email-form")
+  if (emailForm) {
+    emailForm.addEventListener("submit", e => {
+      e.preventDefault()
+      processForm(emailForm)
+    })
+  }
+  return (
+    <>
+      <div id="newsletter" />
+      <section className="section modular-features offset-box">
+        <div className="container grid-sm">
+          <div className="frame-box without-offset-top columns justify-center">
+            <h3 className="col-12">Newsletter</h3>
+            <div className="column col-8 col-md-8">
+              <p>
+                Subscribe to the newsletter to receive my posts, news and more
+                things right to your inbox.{" "}
+                <span role="img" aria-label="Newsletter">
+                  🚀
+                </span>
+                <br />
+                (content in{" "}
+                <span role="img" aria-label="Flag">
+                  🇪🇸
+                </span>{" "}
+                )
+              </p>
+              <form
+                className="email-form form"
+                name="newsletter"
+                method="POST"
+                data-netlify="true"
+                netlify-honeypot="bot-field"
+              >
+                <div hidden aria-hidden="true">
+                  <label>
+                    Don’t fill this out if you're human:
+                    <input name="bot-field" />
+                  </label>
+                </div>
+                <div className="input-group">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Your email"
+                    id="email"
+                    required
+                    className="form-input input-lg mb-2"
+                  />
+                  <button type="submit" className="btn btn-primary btn-lg">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Newsletter
