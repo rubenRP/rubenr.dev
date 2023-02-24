42160b6cd1a8ac5889eb4b4cb04cdff8e46694dc
Rubén Rodríguez <ruben.rodriguezpaz@gmail.com>
2023-02-24 09:53:26 +0100

    feat: :sparkles: Ackee added (#61)
    https://github.com/rubenRP/rubenr.dev/issues/47
    
    Co-authored-by: Ruben Rodriguez <hola@rubenr.dev>

---
+++ components/Ackee.vue
@@ -0,0 +1,10 @@
+<template></template>
+<script setup lang="ts">
+import * as ackeeTracker from "ackee-tracker";
+
+const rConfig = useRuntimeConfig();
+
+ackeeTracker
+  .create(rConfig.ackee.server, rConfig.ackee.options)
+  .record(rConfig.ackee.domainId);
+</script>
