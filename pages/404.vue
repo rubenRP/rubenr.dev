fda031c3ebc0ffef764d9588521b080005ea1c56
Ruben Rodriguez <ruben.rodriguez@scalefast.com>
2023-03-01 15:35:43 +0100

    fix: 404 redirect fix

---
+++ pages/404.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <section id="start" />
+    <section id="body-wrapper" class="section blog-listing">
+      <div class="container grid-md">
+        <div class="columns">
+          <div id="item" class="column col-12">
+            <div class="content-item h-entry">
+              <div class="content-title text-center">
+                <h2 class="p-name mt-1">Not Found</h2>
+              </div>
+
+              <div class="e-content text-center">
+                <p class="mb-5">
+                  You just hit a route that doesn&#39;t exist... the sadness.
+                </p>
+                <nuxt-img src="/img/404.gif" alt="404" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+const bodyClasses = "header-fixed header-animated";
+
+// Clear body classes first
+useHead({
+  bodyAttrs: {
+    class: "",
+  },
+});
+
+useHead({
+  bodyAttrs: {
+    class: bodyClasses,
+  },
+});
+</script>
