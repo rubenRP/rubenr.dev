d3481a72a30fe45c54b002eeddb0e13f3b659748
Ruben Rodriguez <hola@rubenr.dev>
2025-03-17 09:27:03 +0100

    Astro test

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), mdx()]
+});
