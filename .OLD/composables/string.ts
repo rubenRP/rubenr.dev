84b4d51648ea829abcdfe823086f3527925e1272
Ruben Rodriguez <hola@rubenr.dev>
2025-03-21 12:53:22 +0100

    Clean OlD

--- .OLD/composables/string.ts
+++
@@ -1,1 +0,0 @@
-export const replaceSpaces = (str: string) => str.replace(/\s/g, "-");
--- .OLD/pages/blog/tag/index.vue
+++
@@ -1,6 +0,0 @@
-<template></template>
-<script setup lang="ts">
-import { useRouter } from "vue-router";
-const router = useRouter();
-router.push("/blog");
-</script>
